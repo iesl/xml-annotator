643b91773ac75a636eb21775c8ef6e18dbd4b15b
Thomas Logan <thomaslilogan@gmail.com>
2014-12-18 11:58:53 -0500

    created tests for getTransformedCoords

--- src/test/scala/AnnotatorSpec.scala
+++ src/test/scala/AnnotatorSpec.scala
@@ -5,7 +5,126 @@ import org.scalatest._
 import scala.collection.immutable.IntMap
 import scala.collection.immutable.Set
 
+import org.jdom2.filter.ElementFilter
+import org.jdom2.Element
+import org.jdom2.Document
+import org.jdom2.util.IteratorIterable
+
 class AnnotatorSpec extends FunSuite {
+
+  val e = {
+    new Element("svg")
+      .setAttribute("version", "1.1") 
+      .setAttribute("width", "612px")
+      .setAttribute("height", "3168px")
+      .setAttribute("viewBox", "0 0 612 3168")
+  }
+
+  val e_1 = {
+    val _e = new Element("g")
+      .setAttribute("transform", "matrix(1 0 0 -1 0 792)")
+    e.addContent(_e)
+    _e
+  } 
+
+  val e_1_1 = {
+    val _e = new Element("text")
+      .setAttribute("transform", "matrix(0 1 -1 0 32 256) scale(1, -1)")
+    e_1.addContent(_e)
+    _e
+  }
+
+  val e_1_1_1 = {
+    val _e = new Element("tspan")
+      .setAttribute("x", "0 8.88 15.54 29.98 35.54 45.54 51.1 61.1 71.1 81.1 91.1 96.1")
+      .setAttribute("endX", "100.2")
+      .setAttribute("y", "0")
+      .setText("abcdefghijkl")
+    e_1_1.addContent(_e)
+    _e
+  }
+
+  val e_1_2 = {
+    val _e = new Element("text")
+      .setAttribute("transform", "translate(136.8 669.12) scale(1, -1)")
+    e_1.addContent(_e)
+    _e
+  }
+
+  val e_1_2_1 = {
+    val _e = new Element("tspan")
+      .setAttribute("x", "0 11.51 20.15 25.91 33.59 43.19 48.93 53.03 65.51 73.19 77.99 86.63 92.39 97.19 105.83")
+      .setAttribute("endX", "112.43")
+      .setAttribute("y", "0")
+      .setText("abcdefghijklmno")
+    e_1_2.addContent(_e)
+    _e
+  }
+
+  val e_1_2_2 = {
+    val _e = new Element("tspan")
+      .setAttribute("x", "137.16 146.75 154.43 164.03 171.71 186.11")
+      .setAttribute("endX", "191.22")
+      .setAttribute("y", "19.91")
+      .setText("abcdef")
+    e_1_2.addContent(_e)
+    _e
+  }
+
+  test("getTransformedCoords") {
+
+    intercept[NullPointerException] {
+      Annotator.getTransformedCoords(e_1_1, e)
+    }
+
+    assertResult(
+      Annotator.PositionGroup(
+        List(32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0),
+        32.0,
+        List(536.0, 527.12, 520.46, 506.02, 500.46, 490.46, 484.9, 474.9, 464.9, 454.9, 444.9, 439.9)
+      )
+    ) {
+      Annotator.getTransformedCoords(e_1_1_1, e)
+    }
+
+    assertResult(
+      Annotator.PositionGroup(
+        List(32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0, 32.0),
+        32.0,
+        List(256.0, 264.88, 271.54, 285.98, 291.54, 301.54, 307.1, 317.1, 327.1, 337.1, 347.1, 352.1)
+      )
+    ) {
+      Annotator.getTransformedCoords(e_1_1_1, e_1_1)
+    }
+
+    
+    assertResult(
+      Annotator.PositionGroup(
+        List(137.16, 146.75, 154.43, 164.03, 171.71, 186.11),
+        191.22,
+        List(19.91, 19.91, 19.91, 19.91, 19.91, 19.91)
+      )
+    ) {
+      Annotator.getTransformedCoords(e_1_2_2, e_1_2_2)
+    }
+
+    assertResult(
+      Annotator.PositionGroup(
+        List(273.96000000000004, 283.55, 291.23, 300.83000000000004, 308.51, 322.91),
+        328.02,
+        List(142.79, 142.79, 142.79, 142.79, 142.79, 142.79)
+      )
+    ) {
+      Annotator.getTransformedCoords(e_1_2_2, e)
+    }
+
+    assertResult(
+      Annotator.getTransformedCoords(e_1_2_2, e)
+    ) {
+      Annotator.getTransformedCoords(e_1_2_2, e_1)
+    }
+
+  }
 
   test("mkIndexPairSeq") {
     assertResult(
